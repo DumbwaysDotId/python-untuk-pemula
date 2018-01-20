ead617601c33b1c296e6c4e155e89e964ac6170d
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-01-20 07:03:27 +0700

    3. variable

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("hello world dumbways.id")
+# string, integer, double, float, boolean, array, obj
+
+# string
+x = "x"
+food = "apple pie"
+my_favourite_food = "cheese burger"
+myFavouriteFood = "kebab"
+
+# integer
+number = 1
+first_number = 2
+second_number = 3
+
+# print(second_number + first_number)
+
+# double dan float
+decimal = 1.2
+my_decimal = 3.3
+
+# print(decimal + my_decimal)
+
+# boolean
+is_clean = True
+
+print(is_clean or False)
