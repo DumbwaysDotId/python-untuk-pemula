752c98fd37809943f8600c49ead499af388293f1
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-01-27 13:29:19 +0700

    10.class

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-# method/function
+# class
+class Employee():
+    name = "Joko"
 
-operator = raw_input("Please input operator (+ - / *): ")
-first_number = raw_input("#1: ")
-second_number = raw_input("#2: ")
-first_number = int(first_number)
-second_number = int(second_number)
+    def get_name(self):
+        return self.name
 
-def calculate(operator, number1, number2):
-    if operator == "+":
-        return number1 + number2
-    elif operator == "-":
-        return number1 - number2
-    elif operator == "/":
-        return number1 / number2
-    else:
-        return number1 * number2
-
-print(calculate(operator, first_number, second_number))
+#instantiate class
+employee = Employee()
+employee.name = "Luna"
+print(employee.get_name())
