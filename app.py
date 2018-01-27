f74eb37da751c31750e2c0c3a1a96d93de967e75
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-01-27 13:55:00 +0700

    13.class inheritance

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 # class
-class Employee():
+class Person():
+    name = "Joko"
+    age = 10
+    gender = "male"
+
+    def get_personality(self):
+        return "bad"
+
+
+class Employee(Person):
     def __init__(self, **kwargs):
         k = kwargs
-        self.name = k['name']
         self.salary = k['salary']
-        self.gender = k['gender']
-        self.age = k['age']
 
     def get_name(self):
         return self.name
 
 #instantiate class
-employee = Employee(salary=1000, gender="female", age=27, name="Luna")
-print(
- "%s salary is: %s, gender is %s, and %s years old" %
- (employee.name, employee.salary, employee.gender, employee.age)
-)
+employee = Employee(salary=1000)
+employee.name = "Samohong"
+print(employee.name, employee.salary, employee.get_personality())
